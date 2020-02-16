d5ad040b88650b3e6391ea0a362d59f3272e92c6
Evan Thomas <evan@evanjt.com>
2020-02-17 10:48:07 +1100

    Conform to PEP8

--- closestgeoname.py
+++ closestgeoname.py
@@ -13,58 +13,27 @@ import urllib.request
 import time
 import sys
 from zipfile import ZipFile
+import closestgeoname.constants as constants
 
-# Schema of geonames databases from
-# as of 07/01/2020
-CITY_COLNAMES = ['Geonameid',
-                'Name',
-                'Asciiname',
-                'Alternatenames',
-                'Latitude',
-                'Longitude',
-                'FeatureClass',
-                'FeatureCode',
-                'CountryCode',
-                'Cc2',
-                'Admin1Code',
-                'Admin2Code',
-                'Admin3Code',
-                'Admin4Code',
-                'Population',
-                'Elevation',
-                'Dem',
-                'Timezone',
-                'ModificationDate']
-DBFILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'geonames.sqlite')
-DBNAMES_LINKS = [
-    ("http://download.geonames.org/export/dump/allCountries.zip",
-     "all countries combined in one file (HUGE! > 1.2 GB)"),
-    ("http://download.geonames.org/export/dump/cities500.zip",
-     "all cities with a population > 500 or seats of adm div down to PPLA4 (ca 185.000)"),
-    ("http://download.geonames.org/export/dump/cities1000.zip",
-     "all cities with a population > 1000 or seats of adm div down to PPLA3 (ca 130.000)"),
-    ("http://download.geonames.org/export/dump/cities5000.zip",
-     "all cities with a population > 5000 or PPLA (ca 50.000)"),
-    ("http://download.geonames.org/export/dump/cities15000.zip",
-     "all cities with a population > 15000 or capitals (ca 25.000)")
-    ]
-MIN_QUERY_DIST = 0.1 # Metres
-STATE_COLNAMES = ["AdCode", "State", "StateClean", "ID"]
 
-def import_dump(city_filename, admin_filename, country_filename, city_colnames, state_colnames, encoding='utf-8', delimiter='\t'):
-    MULTIPLIER = 1.4 # Estimated final DB size versus original input
-    filesize = os.stat(city_filename).st_size/1048576 # In MB
+def import_dump(city_filename, admin_filename, country_filename,
+                city_colnames, state_colnames, encoding='utf-8',
+                delimiter='\t'):
+    MULTIPLIER = 1.4  # Estimated final DB size versus original input
+    filesize = os.stat(city_filename).st_size/1048576  # In MB
     print("Initial filesize\t{} MB\nExpected database size\t{} MB\n".format(
-                            round(filesize,2), round(filesize*MULTIPLIER,2)))
+                            round(filesize, 2), round(filesize*MULTIPLIER, 2)))
 
     df = pd.read_csv(city_filename, delimiter=delimiter, encoding=encoding,
                      header=None, names=city_colnames, low_memory=False)
 
     # Filter only the necessary columns
-    cities = df[['Geonameid', 'Name', 'Latitude', 'Longitude', 'Admin1Code', 'CountryCode']]
+    cities = df[['Geonameid', 'Name', 'Latitude', 'Longitude',
+                 'Admin1Code', 'CountryCode']]
 
     # Admin 1 table (commonly the state names)
-    states = pd.read_csv(admin_filename, encoding='utf-8', delimiter='\t', header=None, names=state_colnames)
+    states = pd.read_csv(admin_filename, encoding='utf-8', delimiter='\t',
+                         header=None, names=state_colnames)
 
     # Split the Country/Admin code by the separating decimal
     splitdf = states['AdCode'].str.split(".", expand=True)
@@ -72,16 +41,21 @@ def import_dump(city_filename, admin_filename, country_filename, city_colnames, 
     states['AdminCode'] = splitdf[1]
     states = states[['State', 'CountryID', 'AdminCode']]
 
-    # This file is a bit messier, skip the first 51 rows, only use columns 0 and 4
-    countries = pd.read_csv(country_filename, encoding='utf-8', delimiter='\t', header=None, skiprows=51)
+    # This file is a bit messier, skip first 51 rows, only use columns 0 & 4
+    countries = pd.read_csv(country_filename, encoding='utf-8',
+                            delimiter='\t', header=None, skiprows=51)
     countries = countries[[0, 4]]
     countries.columns = ['ISO', 'Country']
-    states = pd.merge(states, countries, left_on='CountryID', right_on='ISO', how='left')
+    states = pd.merge(states, countries, left_on='CountryID', right_on='ISO',
+                      how='left')
 
     return cities, states, countries
 
+
 def query_db_size(db_path):
-    print("Database size {} MB\n".format(round(os.stat(db_path).st_size/1048576, 2)))
+    print("Database size {} MB\n".format(
+        round(os.stat(db_path).st_size/1048576, 2)))
+
 
 def generate_db(db_path, cities, states, countries):
     with sqlite3.connect(db_path) as conn:
@@ -129,28 +103,31 @@ def generate_db(db_path, cities, states, countries):
         print("Done")
         query_db_size(db_path)
 
-def query_closest_city(db_path, latitude, longitude, epsg=4326, query_buffer_distance=0.1):
-    # Start the buffer size for searching nearest points at a low number for speed,
-    # but keep iterating (doubling distance in size) until somewhere is found.
-    # Hence, this is faster for huge datasets as less points are considered in the spatial
-    # query, but slower for small datasets as more iterations will need to occur if no point
-    # exists
+
+def query_closest_city(db_path, latitude, longitude, epsg=4326,
+                       query_buffer_distance=0.1):
+    ''' Start the buffer size for searching nearest points at a low number
+    for speed, but keep iterating (doubling distance in size) until
+    somewhere is found. Hence, this is faster for huge datasets as
+    less points are considered in the spatial query, but slower for small
+    datasets as more iterations will need to occur if no pointexists
+    '''
 
     row = None
     while row is None:
         # Prevent an infinite loop
-        if query_buffer_distance > 12756 * 1000: # 12,756 km Longest distance on earth
-            print("Distance more than length of earth. Never going to find this point")
+        if query_buffer_distance > 12756 * 1000:  # Longest distance on earth
+            print("Search distance more than length of earth")
             return None
 
         # Form tuple to represent values missing in SQL query
         query_tuple = (longitude,
-                    latitude,
-                    epsg,
-                    longitude,
-                    latitude,
-                    epsg,
-                    query_buffer_distance)
+                       latitude,
+                       epsg,
+                       longitude,
+                       latitude,
+                       epsg,
+                       query_buffer_distance)
         with sqlite3.connect(db_path) as conn:
             conn.enable_load_extension(True)
             conn.load_extension("mod_spatialite")
@@ -183,6 +160,7 @@ def query_closest_city(db_path, latitude, longitude, epsg=4326, query_buffer_dis
 
     return row
 
+
 # Reporthook function to show file download progress. Code from
 # https://blog.shichao.io/2012/10/04/progress_speed_indicator_for_urlretrieve_in_python.html
 def reporthook(count, block_size, total_size):
@@ -195,25 +173,33 @@ def reporthook(count, block_size, total_size):
     speed = int(progress_size / (1024 * duration))
     percent = int(count * block_size * 100 / total_size)
     sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
-                    (percent, progress_size / (1024 * 1024), speed, duration))
+                     (percent, progress_size / (1024 * 1024), speed, duration))
     sys.stdout.flush()
+
 
 def fetch_data(options, choiceid, zipname, admincodes, countryinfo):
     urllib.request.urlretrieve(options[choiceid][0], zipname, reporthook)
-    urllib.request.urlretrieve("http://download.geonames.org/export/dump/countryInfo.txt", "countryInfo.txt", reporthook)
-    urllib.request.urlretrieve("http://download.geonames.org/export/dump/admin1CodesASCII.txt", "admin1CodesASCII.txt", reporthook)
+    urllib.request.urlretrieve(
+        constants.COUNTRYINFO,
+        "countryInfo.txt", reporthook)
+    urllib.request.urlretrieve(
+        constants.ADMINCODES,
+        "admin1CodesASCII.txt", reporthook)
 
 
 # Fetches the dataset from the geonames repository and generates the db
-def download_dataset(city_colnames, state_colnames, db_path, options, choice=None):
-    # If not explicitly defined in function, let the user choose which file to download
-    if (choice is None) or (choice < 1) or (choice > 4):
+def download_dataset(city_colnames, state_colnames, db_path,
+                     options, choice=None):
+    # If not defined in function, let user choose which file to download
+    if (choice is None) or (choice < 0) or (choice > 4):
         for id, option in enumerate(options):
-            print("[{}] {}:\t{}".format(id, option[0].split('/')[-1], option[1]))
+            print("[{}] {}:\t{}".format(id, option[0].split('/')[-1],
+                                        option[1]))
         try:
             choice = int(input("Choose which file to download: "))
             if choice < 0 or choice >= len(options):
-                exit("Error: Choose between {} and {}".format(0, len(options)-1))
+                exit("Error: Choose between {} and {}".format(0,
+                                                              len(options)-1))
         except ValueError:
             exit('Error: Not an integer')
 
@@ -225,7 +211,9 @@ def download_dataset(city_colnames, state_colnames, db_path, options, choice=Non
 
     print()
     # Create database
-    cities, states, countries = import_dump(extracted_txt, admincodes, countryinfo, city_colnames, state_colnames)
+    cities, states, countries = import_dump(extracted_txt, admincodes,
+                                            countryinfo, city_colnames,
+                                            state_colnames)
     generate_db(db_path, cities, states, countries)
 
     # Remove downloaded files
@@ -234,6 +222,7 @@ def download_dataset(city_colnames, state_colnames, db_path, options, choice=Non
     os.remove(countryinfo)
     os.remove(admincodes)
     print("Success")
+
 
 def extract_zip(filename):
     with ZipFile('rawdata.zip', 'r') as zipObj:
@@ -247,28 +236,44 @@ def extract_zip(filename):
                 filename = fileName
     return filename
 
-def check_db_existance(dbfilename, columns_city, columns_state, options):
+
+def check_db_existance(dbfilename, columns_city, columns_state,
+                       options, choice=None):
     if os.path.exists(dbfilename):
         return True
     else:
-        print("GeoNames database", dbfilename, "does not exist. Choose an option")
+        print("GeoNames database", dbfilename,
+              "does not exist. Choose an option")
         try:
-            download_dataset(columns_city, columns_state, dbfilename, options)
-        except:
+            download_dataset(columns_city, columns_state, dbfilename,
+                             options, choice=None)
+        except ValueError:
             return False
         return True
 
+
 def main():
-    if check_db_existance(DBFILENAME, CITY_COLNAMES, STATE_COLNAMES, DBNAMES_LINKS):
+    if check_db_existance(constants.DBFILENAME,
+                          constants.CITY_COLNAMES,
+                          constants.STATE_COLNAMES,
+                          constants.DBNAMES_LINKS):
         parser = argparse.ArgumentParser()
-        parser.add_argument("--database", type=str, help="Set the file for database (default: {})".format(DBFILENAME), default=DBFILENAME)
-        parser.add_argument("longitude", type=float, help="X coordinate (Longitude)")
-        parser.add_argument("latitude", type=float, help="Y coordinate (Latitude)")
+        parser.add_argument("--database", type=str,
+                            help="Set file for database (default: {})".format(
+                                constants.DBFILENAME),
+                            default=constants.DBFILENAME)
+        parser.add_argument("longitude", type=float,
+                            help="X coordinate (Longitude)")
+        parser.add_argument("latitude", type=float,
+                            help="Y coordinate (Latitude)")
         args = parser.parse_args()
 
-        result = query_closest_city(args.database, args.latitude, args.longitude,
-                                        query_buffer_distance=MIN_QUERY_DIST)
+        result = query_closest_city(args.database, args.latitude,
+                                    args.longitude,
+                                    constants.PROJECTION_EPSG,
+                                    constants.MIN_QUERY_DIST)
         print("{}, {}, {}".format(result[0], result[1], result[2]))
+
 
 if __name__ == "__main__":
     main()
--- unittests.py
+++ unittests.py
@@ -5,14 +5,16 @@ import closestgeoname
 import urllib
 import os
 import requests
+import closestgeoname.constants as constants
+
 
 class TestSchema(unittest.TestCase):
     def setUp(self):
         self.countryinfo = "countryInfo.txt"
 
-    # Test the schema of the country info file by asserting the first line considered is at Andorra
+    # Test the country info file by asserting first line is at Andorra
     def test_first_country(self):
-        urllib.request.urlretrieve("http://download.geonames.org/export/dump/countryInfo.txt", self.countryinfo)
+        urllib.request.urlretrieve(constants.COUNTRYINFO, self.countryinfo)
 
         with open(self.countryinfo) as country_file:
             country_info = country_file.readlines()
@@ -22,11 +24,13 @@ class TestSchema(unittest.TestCase):
         country_file_L51 = country_info[51].split("\t")
         self.assertEqual(country_file_L51[0], "AD")
 
-    # Test the download links are available by checking their HTTP header status is 200
+    # Test download links available by checking their HTTP header status 200
     def test_download_links(self):
         for download_link in closestgeoname.DBNAMES_LINKS:
             link_request = requests.head(download_link[0])
-            self.assertEqual(link_request.status_code, 200, "Download link missing")
+            self.assertEqual(link_request.status_code, 200,
+                             "Download link missing")
+
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
@@ -34,7 +38,7 @@ class TestDatabase(unittest.TestCase):
         self.admincodes = "admin1CodesASCII.txt"
         self.countryinfo = "countryInfo.txt"
         self.db_path = "geonames_tester.sqlite"
-        self.min_query_dist = 0.1 # Metres
+        self.min_query_dist = 0.1  # Metres
 
         # Download files
         closestgeoname.fetch_data(closestgeoname.DBNAMES_LINKS,
@@ -42,22 +46,26 @@ class TestDatabase(unittest.TestCase):
                                   self.zipname,
                                   self.admincodes,
                                   self.countryinfo)
-        self.assertTrue(os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                                    self.zipname)))
-        self.assertTrue(os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                                    self.admincodes)))
-        self.assertTrue(os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                                    self.countryinfo)))
+        self.assertTrue(os.path.exists(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            self.zipname)))
+        self.assertTrue(os.path.exists(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            self.admincodes)))
+        self.assertTrue(os.path.exists(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            self.countryinfo)))
 
         # Unzip file
         self.unzipped_filename = closestgeoname.extract_zip(self.zipname)
         self.assertTrue(os.path.exists(self.unzipped_filename))
 
-        cities, states, countries = closestgeoname.import_dump(self.unzipped_filename,
-                                                               self.admincodes,
-                                                               self.countryinfo,
-                                                               closestgeoname.CITY_COLNAMES,
-                                                               closestgeoname.STATE_COLNAMES)
+        cities, states, countries = closestgeoname.import_dump(
+                                            self.unzipped_filename,
+                                            self.admincodes,
+                                            self.countryinfo,
+                                            closestgeoname.CITY_COLNAMES,
+                                            closestgeoname.STATE_COLNAMES)
         self.assertTrue(os.path.exists(self.unzipped_filename))
         closestgeoname.generate_db(self.db_path, cities, states, countries)
         self.assertTrue(os.path.exists(self.db_path))
@@ -70,20 +78,28 @@ class TestDatabase(unittest.TestCase):
         os.remove(self.db_path)
 
     def test_some_cities(self):
-        locations = [(-37.8136, 144.9631, "Melbourne", "Victoria", "Australia"),
-                     (-33.4489, -70.6693, "Santiago", "Santiago Metropolitan", "Chile"),
-                     (51.5074, -0.1278, "London", "England", "United Kingdom"),
-                     (35.6762, 139.6503, "Tokyo", "Tokyo", "Japan"),
-                     (-31.9505, 115.8605, "Perth", "Western Australia", "Australia"),
-                     (40.7128, -74.0060, "New York City", "New York", "United States")]
+        locations = [(-37.8136, 144.9631,
+                      "Melbourne", "Victoria", "Australia"),
+                     (-33.4489, -70.6693,
+                      "Santiago", "Santiago Metropolitan", "Chile"),
+                     (51.5074, -0.1278,
+                      "London", "England", "United Kingdom"),
+                     (35.6762, 139.6503,
+                      "Tokyo", "Tokyo", "Japan"),
+                     (-31.9505, 115.8605,
+                      "Perth", "Western Australia", "Australia"),
+                     (40.7128, -74.0060,
+                      "New York City", "New York", "United States")]
         for location in locations:
-            city, state, country = closestgeoname.query_closest_city(self.db_path,
-                                                location[0],
-                                                location[1],
-                                                query_buffer_distance=self.min_query_dist)
+            city, state, country = closestgeoname.query_closest_city(
+                                    self.db_path,
+                                    location[0],
+                                    location[1],
+                                    query_buffer_distance=self.min_query_dist)
             self.assertEqual(city, location[2])
             self.assertEqual(state, location[3])
             self.assertEqual(country, location[4])
+
 
 if __name__ == '__main__':
     unittest.main()
